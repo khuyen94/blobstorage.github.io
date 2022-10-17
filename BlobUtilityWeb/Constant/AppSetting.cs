5ad5ae873424479f2fd96f8187fe12653a92a260
khuyen94 <khuyen94@gmail.com>
2022-10-17 05:32:15 +0000

    Deploying to main from  @ 9fbc760b5d950ec667607039b44866529b0c9974 🚀

--- BlobUtilityWeb/Constant/AppSetting.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BlobUtilityWeb.Constant
-{
-    public class AppSetting
-    {
-        public static string BlobConnectionString = "";
-        public const string ContainerName = "dec";
-        public const string OutputContainerName = "composition-output";        
-    }
-}
